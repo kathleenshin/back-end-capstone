0724788f24259536c1f9167a97ab296afa34e3ac
kathleen <kathleen.n.shin@gmail.com>
2023-07-31 12:55:23 -0700

    Updated port to 5000

--- server/server.mjs
+++ server/server.mjs
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
+import lists from "./routes/list.mjs";
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/record", records);
+app.use("/list", lists);
 
 // start the Express server
 app.listen(PORT, () => {
