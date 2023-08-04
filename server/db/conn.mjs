558046aa297247285777790cf82979687e107a42
kathleen <kathleen.n.shin@gmail.com>
2023-08-03 20:14:09 -0700

    Updated connection to database

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,16 +1,36 @@
+// import { MongoClient } from "mongodb";
+
+// const connectionString = process.env.ATLAS_URI || "";
+
+// const client = new MongoClient(connectionString);
+
+// let conn;
+// try {
+//     conn = await client.connect();
+// } catch(e) {
+//     console.error(e);
+// }
+
+// let db = conn.db("Cluster1");
+
+// export default db;
+
 import { MongoClient } from "mongodb";
 
 const connectionString = process.env.ATLAS_URI || "";
-
 const client = new MongoClient(connectionString);
 
-let conn;
-try {
-    conn = await client.connect();
-} catch(e) {
-    console.error(e);
+async function setupConnection() {
+    try {
+        await client.connect();
+        const db = client.db("Cluster1");
+        return db;
+    } catch (e) {
+    console.error("Error connecting to MongoDB:", e);
+    return null;
+    }
 }
 
-let db = conn.db("Cluster1");
+const dbPromise = setupConnection();
 
-export default db;
+export default dbPromise;
