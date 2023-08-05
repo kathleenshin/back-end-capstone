283f5a14f19f9d6bd5baebca539f1de7122a9127
Janice <janicenguyenn@gmail.com>
2023-08-05 11:53:19 -0700

    established mongoose connection and created userSchema Co-authored-by: kathleenshin <kathleenshin@users.noreply.github.com>

--- server/models/User.mjs
+++ server/models/User.mjs
@@ -1,10 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    }
-}
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
 
-)
+
+export default mongoose.model('User', userSchema);
