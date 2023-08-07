b2b2546353087f194ca27563dfc8ecf38b8d9091
kathleen <kathleen.n.shin@gmail.com>
2023-08-07 14:29:40 -0700

    Refactored to move db outside of try and catch

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -20,17 +20,27 @@ import { MongoClient } from "mongodb";
 const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
 
-async function setupConnection() {
-    try {
-        await client.connect();
-        const db = client.db("Cluster1");
-        return db;
-    } catch (e) {
-    console.error("Error connecting to MongoDB:", e);
-    return null;
-    }
-}
+// async function setupConnection() {
+//     try {
+//         await client.connect();
+//         const db = client.db("Cluster1");
+//         return db;
+//     } catch (e) {
+//     console.error("Error connecting to MongoDB:", e);
+//     return null;
+//     }
+// }
 
 //const dbPromise = setupConnection();
 
+let conn;
+try {
+    conn = await client.connect();
+} catch (e) {
+    console.error("Error connecting to MongoDB:", e);
+}
+
+let db = conn.db("capstonedb");
+
 export default db;
+
