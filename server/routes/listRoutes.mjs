459b6225d0414a8ce69b65a5827b665444361592
kathleen <kathleen.n.shin@gmail.com>
2023-08-10 23:07:14 -0700

    Fixed response body bug in conditional statement for post routes, deleted comments
    Co-authored-by: janice15 <janice15@users.noreply.github.com>

--- server/routes/listRoutes.mjs
+++ server/routes/listRoutes.mjs
@@ -27,112 +27,10 @@ router.get("/", async (req, res) => {
     }
 });
 
-// // This section will help you get a single list by id
-// router.get("/list/:listId", async (req, res) => {
-//     let collection = await db.collection("restaurants");
-//     let query = {listId: req.params.listId};
-//     let result = await collection.find(query).toArray();
-
-//     if (!result) {
-//         res.status(404).send("Not found");
-//     } else {
-//         res.status(200).send(result);
-//     }
-// });
-// USE THIS IF CURRENT CODE DOESNT WORK:
-// router.get("/list/:listId", async (req, res) => {
-//     try {
-//         let collection = await db.collection("lists");
-//         let query = { listId: req.params.listId };
-//         let result = await collection.findOne(query);
-
-//         if (!result) {
-//             res.status(404).send("Not found");
-//         } else {
-//             // Populate the restaurants associated with the list
-//             const populatedList = await result.populate('restaurants').execPopulate();
-//             res.status(200).send(populatedList);
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while fetching the list' });
-//     }
-// });
-// listRoutes.mjs
-
-// ... (existing imports and code)
-
-// router.get("/list/:listId", async (req, res) => {
-//     try {
-//         const collection = await db.collection("lists");
-//         const query = { listId: req.params.listId };
-//         const result = await collection.findOne(query);
-
-//         if (!result) {
-//             res.status(404).send("Not found");
-//         } else {
-//             // Assuming result.restaurants is an array of restaurant IDs
-//             if (!result.restaurants || result.restaurants.length === 0) {
-//                 res.status(200).send(result);
-//                 return;
-//             }
-
-//             const restaurantCollection = await db.collection("restaurants");
-//             const restaurantIds = result.restaurants; // Assuming you already have valid restaurant IDs
-
-//             // Manually fetch and populate the associated restaurants using restaurant IDs
-//             const restaurants = await restaurantCollection.find({ restaurantId: { $in: restaurantIds } }).toArray();
-
-//             // Assign the fetched restaurants to the list's 'restaurants' field
-//             result.restaurants = restaurants;
-
-//             res.status(200).send(result);
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while fetching the list' });
-//     }
-// });
-// listRoutes.mjs
-
-// ... (existing imports and code)
-
-// router.get("/list/:listId", async (req, res) => {
-//     try {
-//         const collection = await db.collection("lists");
-//         const query = { listId: req.params.listId };
-//         const result = await collection.findOne(query);
-
-//         if (!result) {
-//             res.status(404).send("Not found");
-//         } else {
-//             // Assuming result.restaurants is an array of restaurant IDs
-//             if (!result.restaurants || result.restaurants.length === 0) {
-//                 res.status(200).send(result);
-//                 return;
-//             }
-
-//             const restaurantCollection = await db.collection("restaurants");
-
-//             // Manually fetch and populate the associated restaurants using restaurant IDs
-//             // const restaurants = await restaurantCollection.find({ _id: { $in: result.restaurants } }).toArray();
-//             const restaurants = await restaurantCollection.find({ restaurantId: req.params.restaurantId, listId: req.params.listId}).toArray();
-
-//             // Assign the fetched restaurants to the list's 'restaurants' field
-//             // result.restaurants = restaurants;
-
-//             res.status(200).send(result);
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while fetching the list' });
-//     }
-// });
 
 // This section will help you get a single list by id
 router.get("/list/:listId", async (req, res) => {
     try {
-        //const listId = "e7709794-5c8";
         const listId = req.params.listId
 
         let collection = await db.collection("restaurants");
@@ -162,7 +60,6 @@ router.post("/", async (req, res) => {
     let newList = new PostList({
         listId: generateShortUUID(),
         name: req.body.name,
-        //userId: req.body.userId
     });
 
     let collection = db.collection("lists");
@@ -219,23 +116,6 @@ router.delete("/list/:listId", async (req, res) => {
 // ************* Restaurants routes *************
 
 // // This section will help you get a single record by id
-// router.get("/list/:listId/:restaurantId", async (req, res) => {
-//     let collection = await db.collection("restaurants");
-
-//     const restaurantQuery = { restaurantId: req.params.restaurantId};
-//     let result = await collection.findOne(restaurantQuery);
-
-//     if (!result) {
-//         res.status(404).send("Not found");
-//     } else {
-//         res.status(200).send(result);
-//     }
-// });
-// Retrieve a single restaurant
-// listRoutes.mjs
-
-// ... (existing imports and code)
-
 router.get("/list/:listId/:restaurantId", async (req, res) => {
     try {
         const restaurantCollection = await db.collection("restaurants");
@@ -257,7 +137,6 @@ router.get("/list/:listId/:restaurantId", async (req, res) => {
         res.status(500).json({ message: 'An error occurred while fetching the restaurant' });
     }
 });
-
 
 
 
--- server/routes/searchRoutes.mjs
+++ server/routes/searchRoutes.mjs
@@ -33,13 +33,12 @@ router.get('/:sub', async (req, res) => {
 })
 
 
-
 router.get('/search', async (req, res) => {
     //const { term, latitude, longitude } = req.query;
     const { term } = req.query
     const { name } = req.body;
     // const url = `https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&term=${term}&sort_by=best_match&limit=5`;
-    //const url = `https://api.yelp.com/v3/autocomplete/search?term=${term}&location=${location}&sort_by=best_match&limit=5`;
+    // const url = `https://api.yelp.com/v3/autocomplete/search?term=${term}&location=${location}&sort_by=best_match&limit=5`;
     const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=Seattle&sort_by=best_match&limit=5`;
     const options = {
         method: 'GET',
@@ -59,147 +58,8 @@ router.get('/search', async (req, res) => {
     }
 });
 
-// OLD CODE # 1
+
 // // This section will help you add a new restaurant to a list.
-// router.post("/list/:listId", async (req, res) => {
-//     if (!req.body.restaurantName || req.body.restaurantName.trim() === "") {
-//         res.status(400).send("Restaurant name is required");
-//         return;
-//     }
-
-//     const newRestaurant = new PostRestaurant({
-//         restaurantId: generateShortUUID(),
-//         restaurantName: req.body.restaurantName,
-//         listId: req.params.listId,
-//         cuisine: req.body.cuisine,
-//         phoneNumber: req.body.phoneNumber,
-//         address: req.body.address,
-//         pricePoint: req.body.pricePoint
-//     });
-
-//     let collection = await db.collection("restaurants");
-//     let result = await collection.insertOne(newRestaurant);
-
-//     if (result.acknowledged === false) {
-//         res.status(404).send("Not found");
-//     } else {
-//         res.status(200).send(result);
-//     }
-// });
-
-// OLD CODE # 2
-// // Route to save restaurant data to a list
-// router.post('/', async (req, res) => {
-//     //const restaurantData = req.body.restaurantData; // Assuming this is the data from the Yelp API request
-//     //const listId = req.body.listId; // The ID of the list to which you want to add the restaurant
-
-//     try {
-//         // Check if the favorites list exists
-//         // const existingList = await Lists.findById(listId);
-//         const listName = "Favorites";
-//         let listCollection = await db.collection("lists");
-//         const existingList = await listCollection.findOne({ name: listName });
-
-//         if (existingList) {
-//         // If the list exists, add its ID to the restaurant data
-//         //const newRestaurant = new PostRestaurant();
-//         //await newRestaurant.save();
-
-//         let newRestaurant = new PostRestaurant({
-//             listId: listName.listId,
-//             restaurantName: req.body.name,
-//             cuisine: req.body.categories,
-//             phoneNumber: req.body.display_phone,
-//             address: req.body.display_address,
-//             pricePoint: req.body.price,
-//             restaurantId: generateShortUUID()
-//         });
-    
-//         let collection = db.collection("restaurants");
-//         let result = await collection.insertOne(newRestaurant);
-
-
-//         } else {
-//         // If the list doesn't exist, create a new one
-//         const newList = new PostFavoritesList({
-//             listId: generateShortUUID()
-//         });
-        
-//         let listCollection = await db.collection("lists");
-//         let result = await listCollection.insertOne(newList);
-//         }
-
-//         // Create a new restaurant document using the Restaurant schema
-//         let newRestaurant = new PostRestaurant({
-//             listId: listName.listId,
-//             restaurantName: req.body.name,
-//             cuisine: req.body.categories,
-//             phoneNumber: req.body.display_phone,
-//             address: req.body.display_address,
-//             pricePoint: req.body.price,
-//             restaurantId: generateShortUUID()
-//         });
-
-//         let collection = db.collection("restaurants");
-//         let result = await collection.insertOne(newRestaurant);
-
-//         res.status(201).json({ message: 'Restaurant saved successfully' });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while saving the restaurant' });
-//     }
-//     });
-
-// router.post('/', async (req, res) => {
-//     const listName = "Favorites"; // Name of the list you want to associate with
-
-//     try {
-//         let listCollection = await db.collection("lists");
-//         const existingList = await listCollection.findOne({ name: listName });
-
-//         if (!existingList) {
-//             // If the list doesn't exist, create a new one
-//             const newList = new PostFavoritesList({
-//                 listId: generateShortUUID()
-//             });
-            
-//             await listCollection.insertOne(newList);
-            
-//             // Associate the new list's ID with the restaurant data
-//             req.body.listId = newList.listId;
-//         } else {
-//             // If the list exists, associate its ID with the restaurant data
-//             req.body.listId = existingList.listId;
-//         }
-
-//         // Create a new restaurant instance
-//         const newRestaurant = new PostRestaurant({
-//             listId: req.body.listId, // Use the list ID to populate the reference
-//             restaurantName: req.body.name,
-//             cuisine: req.body.categories,
-//             phoneNumber: req.body.display_phone,
-//             address: req.body.display_address,
-//             pricePoint: req.body.price,
-//             restaurantId: generateShortUUID()
-//         });
-
-//         // Insert the restaurant instance into the restaurants collection
-//         let restaurantCollection = await db.collection("restaurants");
-//         let result = await restaurantCollection.insertOne(newRestaurant);
-
-//         // res.status(201).json({ message: 'Restaurant saved successfully' });
-//         //res.status(201).send(result);
-//         if (!result) {
-//             res.status(204).send("No content");
-//         } else {
-//             res.status(201).send(result);
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while saving the restaurant' });
-//     }
-// });
-
 router.post('/', async (req, res) => {
     const listName = "Favorites"; // Name of the list you want to associate with
 
@@ -237,8 +97,6 @@ router.post('/', async (req, res) => {
         let restaurantCollection = await db.collection("restaurants");
         let result = await restaurantCollection.insertOne(newRestaurant);
 
-        //res.status(201).json({ message: 'Restaurant saved successfully' });
-        //res.status(201).send(result);
         if (!result) {
             res.status(204).send("No content");
             return;
