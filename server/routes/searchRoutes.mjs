8a346275bca28a2bf73dfead450dd79d74a3ddcf
kathleen <kathleen.n.shin@gmail.com>
2023-08-12 21:15:25 -0700

    Reverted test listName back to Favorites instead of test name, removed commented out old code

--- server/routes/searchRoutes.mjs
+++ server/routes/searchRoutes.mjs
@@ -13,8 +13,7 @@ function generateShortUUID() {
     return shortUUID;
 }
 
-// const listName = "Favorites"; // Name of the list you want to associate with
-const listName = "testfavorites";
+const listName = "Favorites"; // Name of the list you want to associate with
 
 router.get('/search', async (req, res) => {
     //const { term, latitude, longitude } = req.query;
@@ -41,178 +40,15 @@ router.get('/search', async (req, res) => {
     }
 });
 
-// CODE ON MAIN
+
 // // This section will help you add a new restaurant to a list.
-// router.post('/', async (req, res) => {
-//     const listName = "Favorites"; // Name of the list you want to associate with
-
-//     try {
-//         let listCollection = await db.collection("lists");
-//         const existingList = await listCollection.findOne({ name: listName });
-
-//         if (!existingList) {
-//             // If the list doesn't exist, create a new one
-//             const newList = new PostFavoritesList({
-//                 listId: generateShortUUID(),
-//                 name: req.body.name,
-//             });
-            
-//             await listCollection.insertOne(newList);
-            
-//             // Associate the new list's ID with the restaurant data
-//             req.body.listId = newList.listId;
-//         } else {
-//             // If the list exists, associate its ID with the restaurant data
-//             req.body.listId = existingList.listId;
-//         }
-
-//         // Create a new restaurant instance
-
-
-    
-//         const newRestaurant = new PostRestaurant({
-//             listId: req.body.listId, // Use the list ID to populate the reference
-//             restaurantName: req.body.name,
-//             cuisine: req.body.cuisine,
-//             phoneNumber: req.body.phoneNumber,
-//             address: req.body.address,
-//             pricePoint: req.body.pricePoint,
-//             restaurantId: generateShortUUID()
-//         });
-
-//         // Insert the restaurant instance into the restaurants collection
-//         let restaurantCollection = await db.collection("restaurants");
-//         let result = await restaurantCollection.insertOne(newRestaurant);
-
-//         if (!result) {
-//             res.status(204).send("No content");
-//             return;
-//         } else {
-//             res.status(201).send(result);
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while saving the restaurant' });
-//     }
-// });
-
-
-// // experiment code 1
-// // This section will help you add a new restaurant to a list.
-// router.post('/search', async (req, res) => {
-    // const listName = "Favorites"; // Name of the list you want to associate with
-
-    // try {
-    //     let listCollection = await db.collection("lists");
-    //     const existingList = await listCollection.findOne({ name: listName });
-
-    //     if (!existingList) {
-    //         // If the list doesn't exist, create a new one
-    //         const newList = new PostFavoritesList({
-    //             listId: generateShortUUID()
-    //         });
-
-    //         await listCollection.insertOne(newList);
-
-    //         // Associate the new list's ID with the restaurant data
-    //         req.body.listId = newList.listId;
-    //     } else {
-    //         // If the list exists, associate its ID with the restaurant data
-    //         req.body.listId = existingList.listId;
-    //     }
-
-//         // Make a Yelp API call to fetch restaurant data
-//         const { term } = req.body;
-
-//         const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=Seattle&sort_by=best_match&limit=5`;
-
-//         const yelpResponse = await fetch(url, {
-//             method: 'GET',
-//             headers: {
-//                 accept: 'application/json',
-//                 Authorization: `Bearer ${process.env.YELP_API_KEY}`
-//             }
-//         });
-
-//         if (!yelpResponse.ok) {
-//             throw new Error('Yelp API request failed');
-//         }
-
-//         const yelpData = await yelpResponse.json();
-
-//         // Assuming yelpData.businesses is an array of businesses
-//         const firstBusiness = yelpData.businesses[0];
-
-//         // Create a new restaurant instance using Yelp data
-//         const newRestaurant = new PostRestaurant({
-//             listId: req.body.listId,
-//             restaurantName: firstBusiness.name,
-//             // Assuming cuisines are in the "categories" array
-//             cuisine: firstBusiness.categories.map(category => category.title).join(', '),
-//             phoneNumber: firstBusiness.display_phone,
-//             address: firstBusiness.location.display_address[0],
-//             pricePoint: firstBusiness.price,
-//             restaurantId: generateShortUUID()
-//         });
-
-//         // Insert the restaurant instance into the restaurants collection
-//         let restaurantCollection = await db.collection("restaurants");
-//         let result = await restaurantCollection.insertOne(newRestaurant);
-
-//         if (!result) {
-//             res.status(204).send("No content");
-//             return;
-//         } else {
-//             res.status(201).send(result);
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while saving the restaurant' });
-//     }
-// });
-
-// // experiment code 2
-// router.post('/search/save-favorite', async (req, res) => {
-//     console.log("Received data: ", req.body);
-
-//     try {
-//         const { listId, restaurantName, cuisine, phoneNumber, address, pricePoint } = req.body;
-
-//       // Create a new restaurant instance
-//         const newRestaurant = {
-//             listId,
-//             restaurantName,
-//             cuisine,
-//             phoneNumber,
-//             address,
-//             pricePoint,
-//             restaurantId: generateShortUUID() // Assuming you have a function to generate UUID
-//         };
-
-//         // Insert the restaurant instance into the restaurants collection
-//         const restaurantCollection = db.collection("restaurants");
-//         const result = await restaurantCollection.insertOne(newRestaurant);
-
-//         if (!result) {
-//             res.status(204).send("No content");
-//         } else {
-//             res.status(201).json({ message: "Restaurant saved to favorites", restaurant: newRestaurant });
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'An error occurred while saving the restaurant' });
-//     }
-//     });
-
-
-// experiment code 3
 router.post('/search/save-favorite', async (req, res) => {
     console.log("Received data: ", req.body);
     
     try {
         const { listId, restaurantName, cuisine, phoneNumber, address, pricePoint } = req.body;
         
-        // Create a new restaurant instance
+    // Create a new restaurant instance
     const newRestaurant = {
         listId: req.body.listId,
         restaurantName: req.body.name, // Access the restaurant name from the request body
@@ -258,7 +94,6 @@ router.post('/search/save-favorite', async (req, res) => {
         res.status(500).json({ message: 'An error occurred while saving the restaurant' });
     }
 });
-
 
 
 export default router;
