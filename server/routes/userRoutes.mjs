d10028b6558e103c22a97e88c79f0b9da93a1e3c
Janice <janicenguyenn@gmail.com>
2023-08-14 17:17:58 -0700

    added user model and created routes to save google sub id

---
+++ server/routes/userRoutes.mjs
@@ -0,0 +1,46 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import User from "../models/User.mjs";
+
+const router = express.Router();
+
+// This section will help you get a list of all the lists.
+router.get("/users", async (req, res) => {
+    let collection = await db.collection("users");
+    let result = await collection.find({}).toArray();
+    
+    if (!result) {
+        res.status(404).send("User Not found");
+    } else {
+        res.status(200).send(result);
+    }
+});
+router.post("/save-user", async (req, res) => {
+    console.log("Received request:", req.body)
+    const subId = req.body.subId;
+
+    try {
+    const existingUser = await User.findOne({ subId });
+    if (existingUser) {
+        res.status(200).json({ message: "User already exists" });
+    } else {
+        const newUser = new User({
+        subId: req.body.subId,
+        });
+        const UserCollection = db.collection("users");
+        const result = await UserCollection.insertOne(newUser);
+        
+        if (!result) {
+        res.status(204).send("No content");
+        } else {
+        res.status(201).json({ message: "User saved to database", user: newUser });
+        }
+    }
+    } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+
+export default router;
+
--- server/server.mjs
+++ server/server.mjs
@@ -4,6 +4,7 @@ import "./loadEnvironment.mjs";
 
 import searchRoutes from "./routes/searchRoutes.mjs"
 import listRoutes from "./routes/listRoutes.mjs"
+import userRoutes from "./routes/userRoutes.mjs";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/dashboard", listRoutes);
 app.use("/",searchRoutes);
+app.use("/dashboard/user", userRoutes)
 // start the Express server
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
