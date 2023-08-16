27fe9fccd513b344ee96eedde8748427f71aac3a
Janice <janicenguyenn@gmail.com>
2023-08-16 09:06:16 -0700

    commented out favorites model

--- server/models/Favorites.mjs
+++ server/models/Favorites.mjs
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
 
-const favoritesSchema = mongoose.Schema({
-    listName: {
-        type: String,
-        default: "Favorites"
-    },
-    listId: String,
-    //userId: String
+// const favoritesSchema = mongoose.Schema({
+//     listName: {
+//         type: String,
+//         default: "Favorites"
+//     },
+//     listId: String,
+//     //userId: String
     
-})
+// })
 
 
-const PostFavoritesList = mongoose.model('PostFavoritesList', favoritesSchema);
+// const PostFavoritesList = mongoose.model('PostFavoritesList', favoritesSchema);
 
-export default PostFavoritesList;
+// export default PostFavoritesList;
