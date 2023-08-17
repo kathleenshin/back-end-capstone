331a39f62c299923af58bc979dc6da8b9f869bcb
kathleen <kathleen.n.shin@gmail.com>
2023-08-16 23:48:00 -0700

    Updated newRestaurant instance and Restaurant model to include all needed yelp restaurant keys

--- server/models/Restaurant.mjs
+++ server/models/Restaurant.mjs
@@ -6,23 +6,47 @@ const categorySchema = mongoose.Schema({
     title: String
 });
 
+const displayAddressSchema = mongoose.Schema({
+    0: String,
+    1: String
+})
+
 const restaurantSchema = mongoose.Schema({
     yelpId: String,
     name: String,
-    categories: [categorySchema], // Now an array of category objects
-    display_categories: String,
+    categories: [categorySchema],
     display_phone: String,
-    address1: String,
-    address2: String,
-    address3: String,
-    city: String,
-    country: String,
-    display_address: String,
-    state: String,
-    zip_code: String,
+    // address1: String,
+    // address2: String,
+    // address3: String,
+    // city: String,
+    // country: String,
+    // display_address: String,
+    // state: String,
+    // zip_code: String,
+    distance: Number,
+    coordinates: {
+        latitude: Number,
+        longitude: Number,
+    },
+    location: {
+        address1: String,
+        address2: String,
+        address3: String,
+        city: String,
+        country: String,
+        display_address: [displayAddressSchema],
+        state: String,
+        zip_code: String,
+    },
     price: String,
     listId: String,
-    restaurantId: String
+    restaurantId: String,
+    phone: String,
+    rating: Number,
+    transactions: [String],
+    url: String,
+    image_url: String,
 })
 
 
--- server/routes/searchRoutes.mjs
+++ server/routes/searchRoutes.mjs
@@ -48,27 +48,62 @@ router.post('/search/save-favorite', async (req, res) => {
     try {
         // const { yelpId, name, categories, phoneNumber, address, pricePoint } = req.body;
         
-    // Create a new restaurant instance
+    // // Create a new restaurant instance
+    // const newRestaurant = {
+    //     yelpId: req.body.id,
+    //     listId: "42bef239-dc7",
+    //     listName: "Favorites",
+    //     name: req.body.name, // Access the restaurant name from the request body
+    //     // categories: req.body.categories.map(category => category.title).join(', '), // Access categories from the request body
+    //     categories: req.body.categories.map(category => `${category.alias}- ${category.title}`).join(', '),
+    //     display_categories: req.body.categories.map(category => category.title).join(', '), 
+    //     display_phone: req.body.display_phone,
+    //     location: req.body.location.map(address => `${address.address1} ${address.address2} ${address.address3} ${address.city} ${address.country}`),
+    //     address1: req.body.location.address1,
+    //     address2: req.body.location.address2,
+    //     address3: req.body.location.address3,
+    //     city: req.body.location.city,
+    //     country: req.body.location.country,
+    //     display_address: req.body.location.display_address.join(', '), // Access display address from the request body
+    //     state: req.body.location.state,
+    //     zip_code: req.body.location.zip_code,
+    //     price: req.body.price, // Access price from the request body
+    //     restaurantId: generateShortUUID() // Assuming you have a function to generate UUID
+    //     };
     const newRestaurant = {
-        yelpId: req.body.id,
         listId: "42bef239-dc7",
         listName: "Favorites",
-        name: req.body.name, // Access the restaurant name from the request body
-        // categories: req.body.categories.map(category => category.title).join(', '), // Access categories from the request body
-        categories: req.body.categories.map(category => `${category.alias} - ${category.title}`).join(', '),
-        display_categories: req.body.categories.map(category => category.title).join(', '), 
+        categories: req.body.categories.map(category => ({
+            alias: category.alias,
+            title: category.title
+        })),
+        // display_categories: req.body.categories.map(category => category.title).join(', '), 
+        coordinates: req.body.coordinates.map(coordinate => ({
+            latitude: coordinate.latitude,
+            longitude: coordinate.longitude
+        })),
         display_phone: req.body.display_phone,
-        address1: req.body.location.address1,
-        address2: req.body.location.address2,
-        address3: req.body.location.address3,
-        city: req.body.location.city,
-        country: req.body.location.country,
-        display_address: req.body.location.display_address.join(', '), // Access display address from the request body
-        state: req.body.location.state,
-        zip_code: req.body.location.zip_code,
-        price: req.body.price, // Access price from the request body
-        restaurantId: generateShortUUID() // Assuming you have a function to generate UUID
-        };
+        distance: req.body.distance,
+        yelpId: req.body.id,
+        image_url: req.body.image_url,
+        location: req.body.categories.map(address => ({
+            address1: address.address1,
+            address2: address.address2,
+            address3: address.address3,
+            city: address.city,
+            country: address.country,
+            display_address: req.body.location.display_address.join(', '),
+            state: address.state,
+            zip_code: address.zip_code,
+        })),
+        name: req.body.name,
+        phone: req.body.phone,
+        rating: req.body.rating,
+        transactions: req.body.transactions,
+        url: req.body.url,
+        price: req.body.price,
+        restaurantId: generateShortUUID()
+    };
 
         // Check if the "Favorites" list exists
         let listCollection = await db.collection("lists");
